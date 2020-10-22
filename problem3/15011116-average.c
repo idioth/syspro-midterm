acce8db55045bdee0384da970805dc19b8765183
idioth <idioth@ubuntu.localdomain>
2020-10-22 15:21:25 +0900

    problem3 upload

---
+++ problem3/15011116-average.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+  
+int main(void)
+{
+        int system, algorithm, network;
+        float average;
+
+        printf("Input the system programming, algortihm, and network score:");
+        scanf("%d%d%d", &system, &algorithm, &network);
+
+        average = (system + algorithm + network) / 3;
+
+        printf("The average of the three subjects is %.2f.\n", average);
+
+        return 0;
+
+}
+
